5689dae7586db2a4b84da2aa5f93d09c3975c576
dontae1 <brandonium21@gmail.com>
2015-02-20 18:55:27 -0600

    added task

--- task.py
+++ task.py
@@ -1,19 +1,3 @@
-import os
-import subprocess
 import requests
-import sys
-
 task = {'task': 'ls'}
-router = {'router': 'python router.py'}
-# start ROUTER
-def runRouter():
-	result = subprocess.check_output([
-	    item['router']], 
-	    shell=True
-	)
-	print result
-	return result
-# start SERVERS
-
-
-# run TASK
+r = requests.post('http://127.0.0.1:5000/work', data = task)
